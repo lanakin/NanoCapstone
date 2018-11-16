3d7dcc73b03843c0dd446965c61487e56935393c
Lora Anneken <annekenl1@nku18r7016.hh.nku.edu>
2018-11-16 18:17:28 -0500

    Initial commit

---
+++ app/src/main/java/nanodegree/annekenl/walk360/MainActivity.java
@@ -0,0 +1,50 @@
+package nanodegree.annekenl.walk360;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.title_history);
+                    return true;
+                case R.id.navigation_snacks:
+                    mTextMessage.setText(R.string.title_healthy_snacks);
+                    return true;
+                case R.id.navigation_water_calc:
+                    mTextMessage.setText(R.string.title_water_calc);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
