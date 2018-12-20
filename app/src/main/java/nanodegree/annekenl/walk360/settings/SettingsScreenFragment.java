97ba29d7050d43eaa6301a3ad4f2d6a2b24b857d
lanakin <lanneken@gmail.com>
2018-12-20 18:51:44 -0500

    start of retrieving values when settings are set/changed

--- app/src/main/java/nanodegree/annekenl/walk360/settings/SettingsScreenFragment.java
+++ app/src/main/java/nanodegree/annekenl/walk360/settings/SettingsScreenFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import nanodegree.annekenl.walk360.R;
 
@@ -30,10 +34,57 @@ public class SettingsScreenFragment extends PreferenceFragmentCompat implements
         setPreferencesFromResource(R.xml.settings, rootKey);
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+        Preference preference = findPreference(key);
+
+        if (preference != null)
+        {
+            if(key.equals(getResources().getString(R.string.reminders_switch_walk360_key)))
+            {
+                boolean value = sharedPreferences.getBoolean(key, false);
+                Log.d("settings_prefs",value+"");
+            }
+            else if(key.equals(getResources().getString(R.string.days_walk360_key)))
+            {
+                //multi list
+                Set<String> value = new HashSet<String>();
+                value = sharedPreferences.getStringSet(key,value);
+                Log.d("settings_prefs",value.toString());  //days as DAYS enums: sun = 1 to sat = 7
+            }
+            else if(key.equals(getResources().getString(R.string.start_time_walk360_key)))
+            {
+                //time custom pref
+                String value = sharedPreferences.getString(key, "");
+                Log.d("settings_prefs",value); //time string in 24 hour format
+            }
+            else if(key.equals(getResources().getString(R.string.end_time_walk360_key)))
+            {
+                //time custom pref
+                String value = sharedPreferences.getString(key, "");
+                Log.d("settings_prefs",value); //time string in 24 hour format 
+            }
+         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Unregister the listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
 
     @Override
     public void onDisplayPreferenceDialog(Preference preference)
